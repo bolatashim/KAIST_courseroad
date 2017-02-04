b24e7cdb21ce210ead7fd3949d9aa1184c27194c
bolatashim <bolat.ashim@gmail.com>
2017-02-05 02:14:52 +0900

    The website base

---
+++ app.js
@@ -0,0 +1,44 @@
+
+
+var express = require("express");
+var mongoose = require("mongoose");
+var path = require("path");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var flash = require("connect-flash");
+var passport = require("passport");
+
+
+var setUpPassport = require("./setUpPassport");
+var routes = require("./routes");
+
+var app = express();
+mongoose.connect("mongodb://localhost:27017/test");
+setUpPassport();
+
+app.set("port", process.env.PORT || 3000); // setting the port
+app.set("views", path.join(__dirname, "views")); // setting the views folder
+app.set("view engine", "ejs");
+
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use(session({
+  secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(routes);
+
+app.listen(app.get("port"), function() {
+  console.log("Start on port " + app.get("port"));
+});
