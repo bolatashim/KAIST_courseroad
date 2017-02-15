8186601182c992fec761003263826f96cffefbbd
bolatashim <bolat.ashim@gmail.com>
2017-02-16 03:44:29 +0900

    Add courses to mongo through this router

--- routes/courses.js
+++ routes/courses.js
@@ -2,12 +2,31 @@ var express = require("express");
 
 var router = express.Router();
 
-/* print something to the console*/
+var Course = require("../models/course");
+/* print something to the console */
 
 router.get("/", function(req, res, next) {
   console.log("Courses router is up \n");
-  res.redirect("../");
+  //res.redirect("../");
+  next();
 });
 
+/* Making and adding course object to my database. To work with the requests sent through */
+
+router.post("/add", function(req, res) {
+	console.log(req.body.title);
+	res.send(req.body);
+
+	var newCourse = new Course({
+		title: req.body.title,
+		code: req.body.code,
+		depcode: req.body.depcode,
+		llc: req.body.llc,
+		type: req.body.type,
+		year: req.body.year,
+		semester: req.body.semester
+	});
+	newCourse.save();
+});
 
 module.exports = router;
