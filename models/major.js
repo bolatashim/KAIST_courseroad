f991813f52b7cd01263b2c9222bce81a6bf89597
bolatashim <bolat.ashim@gmail.com>
2017-05-02 16:34:48 +0900

    major model changes

--- models/major.js
+++ models/major.js
@@ -31,16 +31,18 @@ year: 2015,   //suppose we talk about guys from 2015
 
 
 //two numbers for each type of courses we have 2 numbers for 1 : 0,1    2: 2, 3    3: 4, 5   4: 6, 7
-//the first number tells about total needed.
-majorReq: ["CS204", "CS206", "CS300", "CS311", "CS320", "CS330", "CS408"]  
+//the first number tells how many credits to be taken from the group. If 0, it means all. Else it 
+//the number of credits to be taken from that specific grouping
 
-majorElect: [a lot ...]
+creditCounts : [ 0, 22, 0, 27, 3, 9, 3, 3 ],
+ 
+majorReq: ["CS204", "CS206", "CS300", "CS311", "CS320", "CS330", "CS408"],
 
-majorReq: []
+majorElect: [],
 
+basicElectReq: [MAS109],
 
-
-
+researchReq: [CS408]
 
 
 
